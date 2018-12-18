292dc4b6e760d1fc19a585d84dfe279c4b94f8dc
home <amirsaleem1990@hotmail.com>
2018-12-18 15:34:25 +0500

    new

--- turtle.py
+++ turtle.py
@@ -1,14 +1,22 @@
 print("Attention: running only on jupyter-notebook\n\n\n\n\n\n\n\n")
+
 import turtle
+import random
 from random import choice
-#wn = turtle.Screen()
-alex= turtle.Turtle()
-for i in range(4):
-    r_l = choice(['left','right'])
-    d = choice(list(range(1, 361)))
-    if r_l == 'left':
-        alex.left(d)
-    else:
-        alex.right(d)
-    alex.forward(choice(list(range(100, 200))))
-turtle.exitonclick()
+def turtle_line():
+    """this functions is drawing 4 random lines """
+    wn = turtle.Screen()
+    wn.bgcolor("lightgreen") # background color
+    alex= turtle.Turtle()
+    alex.pensize(5) # pensize
+    alex.color("hotpink") # turtle color
+    for i in range(4):
+        r_l = choice(['left','right'])
+        d = random.randrange(1, 361)
+        if r_l == 'left':
+            alex.left(d)
+        else:
+            alex.right(d)
+        alex.forward(random.randrange(100, 200))
+    turtle.exitonclick()
+turtle_line()
