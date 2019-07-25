95a08d85f2b191367070ed023c89898e02ab12c3
amirsaleem1990 <amirsaleem1990@hotmail.com>
2019-07-25 14:46:58 +0500

    updated

---
+++ python-monitor-swap-and-ram-usage.py
@@ -0,0 +1,33 @@
+%matplotlib inline
+import psutil
+import pprint
+import pandas as pd
+import datetime
+import time
+def mem_swap():
+    a = psutil.virtual_memory()
+    a = (a.total - a.available) / 1.074e+9
+    b = psutil.swap_memory()
+    b = (b.total - b.free) / 1.074e+9
+    return (a, b)
+
+
+
+
+d = {"Time" : [], "Ram-Used[GB]" : [], "Swap-Used[GB]" : []}
+try: 
+    while True:
+        time.sleep(1)
+        ram, swap = mem_swap()
+        a = datetime.datetime.now()
+        d["Time"].append(':'.join(map(str, [int(a.hour), int(a.minute), int(a.second)])))
+        d["Ram-Used[GB]"].append(round(ram, 2))
+        d["Swap-Used[GB]"].append(round(swap, 2))
+except KeyboardInterrupt:
+    dd = pd.DataFrame(d)
+    print("Max_Ram: ", dd["Ram-Used[GB]"].max())
+    print("Max_Swap: ", dd["Swap-Used[GB]"].max())
+    print("\n")
+    dd.plot()
+    print("\n")
+    pprint.pprint(dd.sort_values("Ram-Used[GB]",ascending=False))
