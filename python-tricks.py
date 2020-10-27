23435023ec316c5bebfbbd12be3296bd1007119f
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-10-27 14:01:34 +0500

    updated

---
+++ python-tricks.py
@@ -0,0 +1,11 @@
+#Print The File Path Of Imported Modules.
+import os 
+import socket 
+  
+print(os) 
+# <module 'os' from '/usr/lib/python3.5/os.py'>
+
+print(socket) 
+# <module 'socket' from '/usr/lib/python3.5/socket.py'>
+
+# --------------------
