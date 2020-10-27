b2464238e9fd03f704ce32cb4cd9771411614491
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-10-27 14:02:40 +0500

    updated

--- python-tricks.py
+++ python-tricks.py
@@ -1,11 +1,20 @@
 #Print The File Path Of Imported Modules.
-import os 
-import socket 
+>>> import os 
+>>> import socket 
   
-print(os) 
+>>> print(os) 
 # <module 'os' from '/usr/lib/python3.5/os.py'>
 
-print(socket) 
+>>> print(socket) 
 # <module 'socket' from '/usr/lib/python3.5/socket.py'>
 
 # --------------------
+# Chaining Of Comparison Operators.
+>>> n = 10
+>>> 1 < n < 20
+# True
+
+>>> 1 > n <= 9
+# False
+
+# ---------------------
