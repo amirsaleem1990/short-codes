ea83cd79bd1eac7ab9474f6513a4ca3ff6512e6e
amirsaleem1990 <amirsaleem1990@hotmail.com>
2020-10-27 15:33:39 +0500

    updated

--- python-tricks.py
+++ python-tricks.py
@@ -65,6 +65,16 @@ itemDictionary = dict(zip(lst_1, lst_2))
 >>> zip(*z)
 # [(1, 2, 3), ('a', 'b', 'c')]
 #--------------------------------
+# Inverting a dictionary using zip
+
+>>> m = {'a': 1, 'b': 2, 'c': 3, 'd': 4}
+>>> m.items()
+# [('a', 1), ('c', 3), ('b', 2), ('d', 4)]
+>>> zip(m.values(), m.keys())
+# [(1, 'a'), (3, 'c'), (2, 'b'), (4, 'd')]
+>>> mi = dict(zip(m.values(), m.keys()))
+>>> mi
+# {1: 'a', 2: 'b', 3: 'c', 4: 'd'}
 
 #--------------------------------
 
