06c567ece58c74ed2900867dbbc98cd46d910e6b
amirsaleem1990 <amirsaleem1990@hotmail.com>
2021-11-01 17:13:02 +0500

    uu

--- core_python.py
+++ core_python.py
@@ -529,3 +529,6 @@ Python modules have a __file__ attribute that tells you the location of their __
 
 # get multiple values from dictionay ............... x = {"a" : 1, "m" : 2, "i" : 3, "r" : 4, "s" : 5}; from operator import itemgetter ; itemgetter("m", 'a', 's')(x) # (2, 1, 5)
 
+# x,y = 8,None ; x or 'amir' #8 ...... y or 'amir' # 'amir'
+
+
